591011ec2c9345aa6b034a5d08c0e8553b083063
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-27 00:55:10 +0530

    Added: Created Linked List Program

---
+++ src/com/bridgelabz/MyLinkedList.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class MyLinkedList {
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList();
+		linkedList.append(50);
+		linkedList.append(25);
+		linkedList.append(70);
+		linkedList.print();
+
+	}
+}
